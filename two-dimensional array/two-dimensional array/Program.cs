a9f5e9cc24fa4b844aa368a24180df0690eeb79e
EGRSkrut1 <EGRSKRUT@gmail.com>
2025-04-27 13:01:26 +0700

    commit
    +two-dimensional array

---
+++ two-dimensional array/two-dimensional array/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter rows:");
+        int rows = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter cols:");
+        int cols = int.Parse(Console.ReadLine());
+        int[,] array = new int[rows, cols];
+
+        Console.WriteLine("enter elements:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"element [{i},{j}]: ");
+                array[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("\narray:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\nreverse array:");
+        for (int i = rows - 1; i >= 0; i--)
+        {
+            for (int j = cols - 1; j >= 0; j--)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
